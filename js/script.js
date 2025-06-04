aa9ad23d907fc10dc84235559b2639b489f057db
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-06-04 14:59:58 +0200

    04.06

--- js/script.js
+++ js/script.js
@@ -4,5 +4,13 @@ jQuery(document).ready(function ($) {
         $(this).next('.faqs-item-content').slideToggle();
     });
 
+    $(".fancybox").fancybox({
+        touch: false,
+        autoFocus: false,
+    });
 
+    let swiper = new Swiper(".swiper-product", {
+        slidesPerView: 4,
+        spaceBetween: 0,
+    });
 });
