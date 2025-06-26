b2631bd031a7b93de37766dbfc363b55b33f1754
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-06-26 19:56:31 +0200

    page

--- wp/mlr/page.php
+++ wp/mlr/page.php
@@ -2,6 +2,19 @@
 
 get_header();
 
-get_template_part('templates/flexible');
+if(get_the_content()) {?>
+
+    <section class="page-content">
+        <div class="container">
+            <h1><?= get_the_title(); ?></h1>
+            <?php the_content(); ?>
+        </div>
+    </section>
+
+<?php }else {
+
+    get_template_part('templates/flexible');
+
+}
 
 get_footer();
