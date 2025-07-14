13429eab224ca70bab8c5acc308a980461fa2928
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-07-14 19:31:47 +0200

    upd

--- wp/mlr/inc/enqueue.php
+++ wp/mlr/inc/enqueue.php
@@ -6,7 +6,7 @@ add_action( 'wp_enqueue_scripts', 'add_scripts' );
 function add_styles() {
     wp_enqueue_style('photoswipe-style', 'https://unpkg.com/photoswipe@5/dist/photoswipe.css', [], '6.8.1');
     wp_enqueue_style('lenis', 'https://unpkg.com/lenis@1.3.4/dist/lenis.css', array(), rand(1111, 9999));
-    wp_enqueue_style('swiper', get_template_directory_uri().'/css/swiper.min.css', array(), rand(1111, 9999));
+    wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), rand(1111, 9999));
     wp_enqueue_style('styles', get_template_directory_uri().'/css/style.css', array(), rand(1111, 9999));
     wp_enqueue_style( 'theme', get_stylesheet_uri() );
 
@@ -20,7 +20,7 @@ function add_scripts() {
     wp_enqueue_script( 'scrolltrigger', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js', array('gsap'), null, true );
 
 
-    wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', [], null, true);
+    wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', [], null, true);
 
     wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array('jquery'), false, true);
 
